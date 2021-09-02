c4d55e0452347d4cbaabeee7673937d022d62f38
Emmanuel Suasnabar <emmanuelsuasnabar@outlook.com>
2021-09-02 13:55:51 -0300

    navbar y reactbootstrap

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+
+
+function NavBar() {
+    return (
+        <div>
+            <p>Anillos</p>
+            <p>Aros</p>
+            <p>Cadenas</p>
+            <p>Pulceras</p>
+        </div>
+    )
+}
+
+export default NavBar
+
